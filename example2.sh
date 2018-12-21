dbcdfa24abab377a7b4aa1d5a6466773229dd042
Xavier Faure <suizokukan@orange.fr>
2018-12-21 21:36:12 +0100

    improved example*.sh, adding the --log option.
    modified:   example1.sh
    modified:   example2.sh

--- example2.sh
+++ example2.sh
@@ -1,1 +1,1 @@
-bin/alpha2aleph_bin --cfgfile=examples/config.ini --symbolsfile=examples/symbols.txt --source=inputfile --outputformat=html --inputfile=examples/input.txt
+bin/alpha2aleph_bin --log=ERROR --cfgfile=examples/config.ini --symbolsfile=examples/symbols.txt --source=inputfile --outputformat=html --inputfile=examples/input.txt
