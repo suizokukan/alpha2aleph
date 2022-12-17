261b2cb67d832e49bcea7d592d3ad23365a6a79d
suizokukan <suizokukan@orange.fr>
2022-12-17 17:25:29 +0100

    new file:   example1bis.sh modified:   examples/symbols.txt

---
+++ example1bis.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "“AabavagaġadaðahawawwazazzaHaṭaṭṭayayyaakalallamammanannasassaʕafapaṣaṣṣaqaqqaraʃaʃʃaθata”" | bin/alpha2aleph_bin --log=ERROR --cfgfile=examples/config.ini --symbolsfile=examples/symbols.txt --source=stdin --outputformat=console
